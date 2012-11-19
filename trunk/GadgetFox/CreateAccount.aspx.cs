62943a1ae38bdd6bb2da3aa79ca1dd5703bf1a79
thang.g.pham@gmail.com <thang.g.pham@gmail.com@bbffe1cb-047b-9cf6-a1ae-e8edb700f647>
2012-11-19 05:04:40 +0000

    Added age confirmation in CreateAccount.aspx.

--- trunk/GadgetFox/CreateAccount.aspx.cs
+++ trunk/GadgetFox/CreateAccount.aspx.cs
@@ -13,32 +13,55 @@ namespace GadgetFox
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
         }
 
         protected void btnCreate_Click(object sender, EventArgs e)
         {
+            DateTime dob = DateTime.Parse(txtDOB.Text);
+            if (dob > DateTime.Today.AddYears(-18))
+            {
+                returnLabel.Text = "You must be 18 or older in order to register!";
+                return;
+            }
+            else
+            {
+                returnLabel.Text = "";
+            }
+
             String myConnectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
             SqlConnection myConnection = new SqlConnection(myConnectionString);
             try
             {
                 myConnection.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO [GadgetFox].[dbo].[Users] ([EmailID],[FirstName],[LastName],[DOB],[Password],[PhoneNum],[RoleId])" +
-                "VALUES(@EmailID,@FirstName,@LastName,@DOB,@Password,@PhoneNum,@RoleId)", myConnection);
-                cmd.Parameters.AddWithValue("@EmailID", txtEmailID.Text);
-                cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
-                cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
-                if (!String.IsNullOrEmpty(txtDOB.Text) && !txtDOB.Text.Equals("MM/DD/YYYY"))
-                    cmd.Parameters.AddWithValue("@DOB", Convert.ToDateTime(txtDOB.Text));
-                else
-                    cmd.Parameters.AddWithValue("@DOB", null);
-                cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
-                cmd.Parameters.AddWithValue("@PhoneNum", String.Format("{0,10:N}",txtPhoneNum.Text));
-                cmd.Parameters.AddWithValue("@RoleID", 1);
-                int rows = cmd.ExecuteNonQuery();
-                if (rows == 1)
+
+                SqlCommand cmd1 = new SqlCommand("Select COUNT(*) from Users where EmailID=@EmailID", myConnection);
+                cmd1.Parameters.AddWithValue("@EmailID", txtEmailID.Text);
+                int idRows = (int)cmd1.ExecuteScalar();
+                if (idRows == 1)
                 {
-                    Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Account created successfully')</SCRIPT>");
-                    Response.Redirect("~/Login.aspx");
+                    returnLabel.Text = "An account with email ID (" + txtEmailID.Text + ") already exists!";
+                }
+                else
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO [GadgetFox].[dbo].[Users] ([EmailID],[FirstName],[LastName],[DOB],[Password],[PhoneNum],[RoleId])" +
+                    "VALUES(@EmailID,@FirstName,@LastName,@DOB,@Password,@PhoneNum,@RoleId)", myConnection);
+                    cmd.Parameters.AddWithValue("@EmailID", txtEmailID.Text);
+                    cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    if (!String.IsNullOrEmpty(txtDOB.Text) && !txtDOB.Text.Equals("MM/DD/YYYY"))
+                        cmd.Parameters.AddWithValue("@DOB", Convert.ToDateTime(txtDOB.Text));
+                    else
+                        cmd.Parameters.AddWithValue("@DOB", null);
+                    cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+                    cmd.Parameters.AddWithValue("@PhoneNum", String.Format("{0,10:N}", txtPhoneNum.Text));
+                    cmd.Parameters.AddWithValue("@RoleID", 1);
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows == 1)
+                    {
+                        Response.Write("<SCRIPT LANGUAGE='JavaScript'>alert('Account created successfully')</SCRIPT>");
+                        Response.Redirect("~/Login.aspx");
+                    }
                 }
             }
             catch (SqlException ex)
--- trunk/GadgetFox/CreateAccount.aspx.designer.cs
+++ trunk/GadgetFox/CreateAccount.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace GadgetFox {
     public partial class CreateAccount {
         
         /// <summary>
+        /// returnLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label returnLabel;
+        
+        /// <summary>
         /// Table1 control.
         /// </summary>
         /// <remarks>
